0e95541607c8a8c03a56908aae17344802b0ec94
evig <bartlomiej.karalus@gmail.com>
2016-04-30 22:34:27 +0100

    Added a first-shot solution.

--- shortest_difference/js/solution.js
+++ shortest_difference/js/solution.js
@@ -1,4 +1,16 @@
 function shortestDifference(valuesArray)
 {
-	return false;
+	var shortest = false;
+	for(var i=0; i<valuesArray.length; i++){
+		for(var j=0; j<valuesArray.length; j++){
+			if(i == j)
+				continue;
+			
+			var difference = Math.abs(valuesArray[i] - valuesArray[j]);
+
+			if(shortest === false || difference < parseInt(shortest))
+				shortest = difference;
+		}
+	}
+	return shortest;
 }
--- shortest_difference/js/test/test.js
+++ shortest_difference/js/test/test.js
@@ -2,8 +2,8 @@
 var testData0 = [];
 var expectedResult0 = false;
 
-QUnit.test("Two players", function( assert ) {
-  assert.ok( shortestDifference(testData0) === expectedResult0, "Passed!" );
+QUnit.test("No players", function( assert ) {
+  assert.ok( shortestDifference(testData0) === expectedResult0, "Wrong!" );
 });
 
 // Just two players
@@ -11,23 +11,23 @@ var testData1 = [7,3];
 var expectedResult1 = 4;
 
 QUnit.test("Two players", function( assert ) {
-  assert.ok( shortestDifference(testData1) === expectedResult1, "Passed!" );
+  assert.ok( shortestDifference(testData1) === expectedResult1, "Wrong!" );
 });
 
 // Many players
-var testData2 = [7,3,12,6,11,14,5,2,4,7,5,6];
+var testData2 = [7,3,12,6,11,14,0,2,4,17,5];
 var expectedResult2 = 1;
 
 QUnit.test("Many players", function( assert ) {
-  assert.ok( shortestDifference(testData2) === expectedResult2, "Passed!" );
+  assert.ok( shortestDifference(testData2) === expectedResult2, "Wrong!" );
 });
 
 // Lots of players
-var testData3 = [40,154,25,10,106,136,175,178,46,145,103,169,178,22,46,178,76,73,25,25,157,49,31,160,196,34,13,82,124,112,52,88,115,7,67,139,193,166,196,118,166,40,52,184,184,106,151,172,175,181,79,127,43,1,160,145,25,82,64,169,142,166,19,16,88,58,175,91,127,58,127,124,190,7,163,31,73,85,13,160,178,1,31,46,169,73,64,4,82,154,70,52,112,109,43,52,196,160,172];
+var testData3 = [262,55,310,340,10,466,463,25,454,436,442,271,244,166,103,226,280,229,34,424,448,82,481,445,283,457,40,253,61,196,1,157,391,190,223,130,472,415,64,160,88,67,316,139,28,100,418,397,475,469,292,334,247,373,46,145,22,451,202,241,370,409,406,58,208,121,151,124,352,112,178,91,118,238,385,220,136,172,460,364,4,109,493,301,484,328,16,214,289,286,217,439,148,400,70,388,106,193,76,265,319,430,487];
 var expectedResult3 = 3;
 
 QUnit.test("Lots of players", function( assert ) {
-  assert.ok( shortestDifference(testData3) === expectedResult3, "Passed!" );
+  assert.ok( shortestDifference(testData3) === expectedResult3, "Wrong!" );
 });
 
 // Almost equal players
@@ -36,5 +36,5 @@ var testData4 = [3,2,3,4,3,2,1,2,1,3,4];
 var expectedResult4 = 0;
 
 QUnit.test("Many players", function( assert ) {
-  assert.ok( shortestDifference(testData4) === expectedResult4, "Passed!" );
+  assert.ok( shortestDifference(testData4) === expectedResult4, "Wrong!" );
 });
