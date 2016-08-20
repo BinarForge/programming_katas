4153533cf9e37b838e186c7ff100e163cadd5c79
BinarForge <bartlomiej.karalus@gmail.com>
2016-08-20 14:42:53 +0100

    Created the skeleton for the Jobber exercise.

---
+++ Jobber/JobberLib/Jobber.cs
@@ -0,0 +1,27 @@
+﻿namespace JobberLib
+{
+	public interface IJobber
+	{
+		void Setup();
+		bool IsValid();
+		string Resolve();
+	}
+
+    public class Jobber : IJobber
+    {
+		public void Setup()
+		{
+
+		}
+
+		public bool IsValid()
+		{
+			return true;
+		}
+
+		public string Resolve()
+		{
+			return string.Empty;
+		}
+    }
+}
---
+++ Jobber/JobberUnitTests/GivenTheJobberAndTheJobList.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using JobberLib;
+
+namespace JobberUnitTests
+{
+	[TestFixture]
+	public class GivenTheJobberClass
+	{
+		[Test]
+		public void TestThatTheClassExists()
+		{
+			var subject = new Jobber();
+			subject.Setup();
+
+			Assert.NotNull(subject, "Jobber class exists");
+		}
+	}
+}
