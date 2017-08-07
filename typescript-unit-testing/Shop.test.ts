ade4ad6d9ffb2558db0968eb9a70a09f5889e41c
BinarForge <contact@binarforge.com>
2017-08-07 22:03:01 +0100

    Added few more tests and the correct implementation of the Shop class following the design by test.

--- typescript-unit-testing/Shop.test.ts
+++ typescript-unit-testing/Shop.test.ts
@@ -33,6 +33,23 @@ describe('Given the Shop instance with few products defined', () => {
         {id: 'banana', price: 2.30}
     ]);
 
+    describe('When purchasing two of each item', () => {
+        shop.addToBasket('apple', 1);
+        shop.addToBasket('apple', 1);
+        shop.addToBasket('banana', 2);
+
+        it('Then still no discounts are applied', () => {
+            expect(shop.getTotal()).to.equal(7.60);
+        })
+    })
+})
+
+describe('Given the Shop instance with few products defined', () => {
+    var shop: Shop = new Shop([
+        {id: 'apple', price: 1.50},
+        {id: 'banana', price: 2.30}
+    ]);
+
     describe('When purchasing three items of the same kind', () => {
         shop.addToBasket('apple', 1);
         shop.addToBasket('banana', 1);
@@ -40,6 +57,27 @@ describe('Given the Shop instance with few products defined', () => {
 
         it('Then the "3 for 2" discount is applied', () => {
             expect(shop.getTotal()).to.equal(5.30);
+        })
+    })
+})
+
+describe('Given the Shop instance with few products defined', () => {
+    var shop: Shop = new Shop([
+        {id: 'apple', price: 1.50},
+        {id: 'banana', price: 2.30}
+    ]);
+
+    describe('When purchasing multiple items qualifying for the discount', () => {
+        shop.addToBasket('apple', 1);
+        shop.addToBasket('apple', 1);
+        shop.addToBasket('apple', 1);
+        shop.addToBasket('apple', 1);
+        shop.addToBasket('apple', 1);
+        shop.addToBasket('banana', 7);
+        shop.addToBasket('apple', 2);
+
+        it('Then the "3 for 2" discount is applied', () => {
+            expect(shop.getTotal()).to.equal(7.50 + 11.50);
         })
     })
 })
--- typescript-unit-testing/Shop.ts
+++ typescript-unit-testing/Shop.ts
@@ -1,14 +1,71 @@
+interface IBasketItem{
+    id: string;
+    price: number;
+    amount?: number;
+}
+
 export default class Shop
 {
-    constructor(inventory: Array<object> = []){
+    basket: Array<IBasketItem> = [];
+    inventory: Array<IBasketItem> = [];
 
+    constructor(inventory: Array<IBasketItem> = []){
+        this.inventory = inventory;
+        this.emptyBasket();
     }
 
-    addToBasket(itemId: string, amount: number): void{
+    private findInInventory(itemId: string): IBasketItem{
+        for(var i: number = 0; i<this.inventory.length; i++){
+            if(this.inventory[i].id === itemId)
+                return this.inventory[i];
+        }
 
+        return null;
+    }
+
+    private positionInBasket(itemId: string): number{
+        for(var i: number = 0; i<this.basket.length; i++){
+            if(this.basket[i].id === itemId)
+                return i;
+        }
+
+        return -1;
+    }
+
+    addToBasket(itemId: string, amount: number = 1): void{
+        var item = this.findInInventory(itemId);
+
+        if(item !== null){
+            var isAlreadyInTheBasket = this.positionInBasket(itemId);
+
+            if(isAlreadyInTheBasket !== -1){
+                this.basket[isAlreadyInTheBasket].amount += amount;
+            }
+            else{
+                item.amount = amount;
+                this.basket.push(item);
+            }
+        }
     }
 
     getTotal(): number{
-        return 0.00;
+        var total: number = 0;
+
+        for(var i: number = 0; i<this.basket.length; i++){        
+            // 3 for 2 discount logic
+            var amount: number = this.basket[i].amount;
+            if(amount > 2){
+                amount -= Math.floor(amount / 3);
+            }
+            // end of: 3 for 2 discount logic
+
+            total += amount * this.basket[i].price;
+        }
+
+        return total;
+    }
+
+    emptyBasket(): void{
+        this.basket = [];
     }
 }
