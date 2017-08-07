47d89fc9ba9dd6801ab2f20c08515485da9f122f
BinarForge <contact@binarforge.com>
2017-08-07 21:38:20 +0100

    Created the Shop class so the test now compiles but it is still red.

---
+++ typescript-unit-testing/Shop.ts
@@ -0,0 +1,14 @@
+export default class Shop
+{
+    constructor(inventory: Array<object> = []){
+
+    }
+
+    addToBasket(itemId: string, amount: number): void{
+
+    }
+
+    getTotal(): number{
+        return 0.00;
+    }
+}
